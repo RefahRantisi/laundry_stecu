bb9d6ee1d56de20d756bb6857c4c2cda54da6790
anniseraa <anniseraa@gmail.com>
2025-12-18 08:47:03 +0700

    update index.php dari sera

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 include 'koneksi.php';
 
 // cek status login
-$login = isset($_SESSION['login']);
+$login = isset($_SESSION['admin_id']);
 
 // ambil data HANYA kalau sudah login
 if ($login) {
